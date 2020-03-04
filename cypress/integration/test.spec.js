1269b8e7349163b2cec0e602559de9cfe0c8eb62
Shawn Axsom <axs221@gmail.com>
2020-03-03 20:11:18 -0500

    initial example tests

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,13 @@
+context("Aliasing", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/commands/aliasing");
+  });
+
+  it(".type() - type into a DOM element", () => {
+    // https://on.cypress.io/type
+    cy
+      .get(".action-email")
+      .type("fake@email.com")
+      .should("have.value", "fake@email.com");
+  });
+});
